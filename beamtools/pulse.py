8582345e61f144a382c95f9aa90c596fa01faf55
Kyle Air <cpkmanchee@gmail.com>
2017-05-12 09:36:41 -0400

    added common.py for generic methods, started pulse.py for pulse char

---
+++ beamtools/pulse.py
@@ -0,0 +1,21 @@
+'''
+Pulse characterization
+
+Created Fri May 12
+
+@author: cpkmanchee
+'''
+
+import numpy as np
+
+from beamtools.constants import h,c,pi
+from beamtools.common import normalize, gaussian, sech2
+
+
+__all__ = ['']
+
+
+def spectrumFT():
+    '''Compute transform limited pulse from spectrum.
+    '''
+    return
