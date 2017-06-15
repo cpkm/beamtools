b3aaf87b5c377191092cb843b101598e22b2792c
Kyle Manchee <cpkmanchee@gmail.com>
2017-06-14 22:33:57 -0400

    Create setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'beamtools',
   packages = ['beamtools'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   platforms=['any'],
   description = 'A suite of tools for the analysis of optical beams',
   author = 'Kyle Manchee',
   author_email = 'cpkmanchee@gmail.com',
   url = 'https://github.com/kikimaroca/beamtools',
-  download_url = 'https://github.com/kikimaroca/beamtools/archive/v0.2.tar.gz',
+  download_url = 'https://github.com/kikimaroca/beamtools/archive/v0.3.tar.gz',
   keywords = ['optics', 'analysis', 'beams', 'spectrum'],
   classifiers = [],
 )
