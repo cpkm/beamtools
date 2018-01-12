af41f519c115d7d8b7d5742ffed79d80760899eb
Kyle Air <cpkmanchee@gmail.com>
2018-01-11 22:21:21 -0500

    build v0.4

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@
 from setuptools import setup
 setup(
   name = 'beamtools',
+  license='MIT',
   packages = ['beamtools'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   platforms=['any'],
   description = 'A suite of tools for the analysis of optical beams',
   author = 'Kyle Manchee',
   author_email = 'cpkmanchee@gmail.com',
   url = 'https://github.com/cpkm/beamtools',
-  download_url = 'https://github.com/cpkm/beamtools/archive/v0.3.tar.gz',
+  download_url = 'https://github.com/cpkm/beamtools/archive/v0.4.tar.gz',
   keywords = ['optics', 'analysis', 'beams', 'spectrum'],
   classifiers = [],
 )
