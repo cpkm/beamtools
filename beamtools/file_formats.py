c0209ac0443a675c320962ba6ded6869b8accd0a
Kyle Air <20861192+cpkm@users.noreply.github.com>
2018-04-14 22:33:26 -0400

    adds pico scope file formats

--- beamtools/file_formats.py
+++ beamtools/file_formats.py
@@ -26,23 +26,6 @@ file_formats = {
                  'delimiter': '\t',
                  'time_format': '%Y/%m/%d %H:%M:%S.%f',
                  },
-            'thorlabs_pm': 
-                {'alias': ['thorlabs_pm','thor','thorlabs','pm100','pm'],
-                 'header_lines': 2,
-                 'number_data_columns': 3,
-                 'column_labels': ['time','power','units'],
-                 'column_units': ['', 'W', ''],
-                 'delimiter': '\t',
-                 'time_format': '%m/%d/%Y %I:%M:%S.%f %p',
-                 },
-            'oceanoptics_spectrometer':
-                {'alias': ['oceanoptics_spectrometer','ocean_optics_spectrometer','oo_spectrometer','oospec','oo_spec','oo'],
-                 'header_lines': 0,
-                 'number_data_columns': 2,
-                 'column_labels': ['wavelength','intensity'],
-                 'column_units': ['nm', 'units'],
-                 'delimiter': ',',
-                 },
             'bt_autocorrelator': 
                 {'alias':['bt_autocorrelator','autocorrelator','ac','auto_correlator','auto','bt_ac','btac'],
                  'header_lines': 0,
@@ -74,7 +57,40 @@ file_formats = {
                  'column_labels': ['position','power'],
                  'column_units': ['um','W'],
                  'delimiter': ',',
-                 }
+                 },
+            'thorlabs_pm': 
+                {'alias': ['thorlabs_pm','thor','thorlabs','pm100','pm'],
+                 'header_lines': 2,
+                 'number_data_columns': 3,
+                 'column_labels': ['time','power','units'],
+                 'column_units': ['', 'W', ''],
+                 'delimiter': '\t',
+                 'time_format': '%m/%d/%Y %I:%M:%S.%f %p',
+                 },
+            'oceanoptics_spectrometer':
+                {'alias': ['oceanoptics_spectrometer','ocean_optics_spectrometer','oo_spectrometer','oospec','oo_spec','oo'],
+                 'header_lines': 0,
+                 'number_data_columns': 2,
+                 'column_labels': ['wavelength','intensity'],
+                 'column_units': ['nm', 'units'],
+                 'delimiter': ',',
+                 },
+            'picoscope': 
+                {'alias': ['picoscope','pico','pico_scope'],
+                 'header_lines': 3,
+                 'number_data_columns': 2,
+                 'column_labels': ['time','volt'],
+                 'column_units': ['s', 'mV'],
+                 'delimiter': ',',
+                 },
+            'picoscope_2ch': 
+                {'alias': ['picoscope_2ch','pico_2ch','pico_scope_2ch','pico2'],
+                 'header_lines': 3,
+                 'number_data_columns': 3,
+                 'column_labels': ['time','voltA','voltB'],
+                 'column_units': ['s', 'mV','mV'],
+                 'delimiter': ',',
+                 },
 
 
             }
